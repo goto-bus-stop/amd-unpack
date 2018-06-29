a785633e5f621a288bbc83bccc6f516fa3a7dbe5
Renée Kooi <renee@kooi.me>
2018-06-29 22:14:24 +0200

    tests

---
+++ test/index.js
@@ -0,0 +1,60 @@
+var test = require('tape')
+var vm = require('vm')
+var bpack = require('browser-pack')
+var concat = require('simple-concat')
+var amdUnpack = require('..')
+
+var abcSrc = `
+  define('a', function () {
+    return 10
+  })
+  define('b', ['a'], function (a) {
+    return a * 2
+  })
+  define('c', ['b', 'a'], function (b, a) {
+    return b / a
+  })
+`
+
+test('defines', function (t) {
+  t.plan(10)
+
+  var modules = amdUnpack(abcSrc)
+
+  t.equal(modules[0].id, 'a')
+  t.equal(modules[1].id, 'b')
+  t.equal(modules[2].id, 'c')
+
+  t.ok(/return 10/.test(modules[0].source))
+  t.ok(/require\("a"\)/.test(modules[1].source))
+  t.ok(/require\("a"\)/.test(modules[2].source))
+  t.ok(/require\("b"\)/.test(modules[2].source))
+
+  t.equal(modules[1].deps.a, 'a')
+  t.equal(modules[2].deps.a, 'a')
+  t.equal(modules[2].deps.b, 'b')
+})
+
+test('commonjs', function (t) {
+  t.plan(2)
+
+  var modules = amdUnpack(abcSrc)
+  var pack = bpack({ raw: true, standalone: 'c', standaloneModule: 'c' })
+  concat(pack, function (err, res) {
+    t.ifError(err)
+    var bundle = res.toString()
+    var ctx = { c: null }
+    vm.runInNewContext(bundle, ctx)
+    t.equal(ctx.c, 2)
+  })
+  modules.forEach(function (m) { pack.write(m) })
+  pack.end()
+})
+
+test('sequence', function (t) {
+  t.plan(3)
+  var modules = amdUnpack('define("a", function(){}), define("b", function() {})')
+  t.equal(modules.length, 2)
+  t.equal(modules[0].id, 'a')
+  t.equal(modules[1].id, 'b')
+})
